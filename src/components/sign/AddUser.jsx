c8b2fdf0433d46479c255f32be82ba3232d0496e
GAMA <abashia.nikoloza@gmail.com>
2022-11-14 20:37:04 +0400

    unilab

--- src/components/sign/AddUser.jsx
+++ src/components/sign/AddUser.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-
+import axios from "axios";
 import UserPhoto from "../../assets/add_a_photo_FILL0_wght400_GRAD0_opsz48.svg";
 
 import "./adduser.scss";
 
 const AddUser = () => {
+  const [image, setImage] = useState("");
+  const [user, setUser] = useState("");
+
+  function uploadImage(e) {
+    setImage(e.target.files[0]);
+  }
+
+  function handleApi(e) {
+    const formData = new FormData();
+    formData.append("image", image);
+    axios
+      .post("https://63725dd6025414c6370ddfdc.mockapi.io/todo", formData)
+      .then((res) => {
+        console.log(res);
+      });
+  }
+
   return (
     <div className="main_container">
       <div className="userContainer">
@@ -13,15 +31,25 @@ const AddUser = () => {
           <div className="user_photo_container">
             <div className="add_photo">add a photo</div>
             <div className="photo_background">
-              <img src={UserPhoto} alt="" />
+              <label htmlFor="fileInput">
+                <img src={UserPhoto} alt="" />
+              </label>
             </div>
+              <button onClick={handleApi} style={{ display: "none" }}>
+                <input
+                  className="upload_input"
+                  type="file"
+                  name="file"
+                  id="fileInput"
+                  onChange={uploadImage}
+                  style={{ display: "none" }}
+                />
+              </button>
           </div>
           <div className="input_fill">fill in you name</div>
-          <input type="text" placeholder="your name" />
+          <input className="user_input" type="text" placeholder="your name" />
           <NavLink to="/todo">
-            <button>
-              <h1>Sign In</h1>
-            </button>
+            <button>Sign In</button>
           </NavLink>
         </div>
       </div>
