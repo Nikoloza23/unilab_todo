a92a94962ee26988813a1f8445759022a58e1a13
GAMA <abashia.nikoloza@gmail.com>
2022-11-16 20:41:11 +0400

    unilab

--- src/components/list/TodoList.jsx
+++ src/components/list/TodoList.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import deletetodo from "../../assets/delete_forever_FILL0_wght400_GRAD0_opsz48.svg";
-import taketodo from "../../assets/done_FILL0_wght400_GRAD0_opsz48 (1).svg";
+import completedtodo from "../../assets/done_FILL0_wght400_GRAD0_opsz48 (1).svg";
 
 import "./todoList.scss";
 
@@ -28,9 +28,22 @@ const TodoList = () => {
     setTaskList(taskList.filter((t) => t.id != id));
   };
 
+  const taskCompleted = (e, id) => {
+    e.preventDefault();
+    const element = taskList.findIndex((elem) => elem.id == id);
+
+    const newTaskList = [...taskList];
+
+    newTaskList[element] = {
+      ...newTaskList[element],
+      isCompleted: true,
+    };
+    setTaskList(newTaskList);
+  };
+
   return (
     <div className="todo_list_container">
-      <h1 className="todo_list_title">Add Your Daily Tasks</h1>
+      <h1 className="task_title">Add Your Daily Tasks</h1>
       <div className="todo_pusher">
         <input
           type="text"
@@ -42,14 +55,17 @@ const TodoList = () => {
       {taskList !== [] ? (
         <ul className="todo_list">
           {taskList.map((t) => (
-            <li>
-              <div className="todo_name">{t.value}</div>
+            <li key={t.id} className={t.isCompleted ? "completed" : "li_style"}>
+              <div className="task_name">{t.value}</div>
               <div className="todo_wrapper">
-                <div className="todo_take">
-                  <img src={taketodo} alt="" />
+                <div
+                  className="completed_task"
+                  onClick={(e) => taskCompleted(e, t.id)}
+                >
+                  <img src={completedtodo} alt="" />
                 </div>
                 <div
-                  className="todo_delete"
+                  className="delete_task"
                   onClick={(e) => deleteTask(e, t.id)}
                 >
                   <img src={deletetodo} alt="" />
